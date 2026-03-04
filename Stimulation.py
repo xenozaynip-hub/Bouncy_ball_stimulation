d45118a7eeb584299f200c20e64244e9296cca2e
xenozaynip <xenozaynip@gmail.com>
2026-03-04 21:29:28 +0100

    feat(code): added circles

--- Stimulation.py
+++ Stimulation.py
@@ -12,6 +12,23 @@ timer = pygame.time.Clock()
 #game variables
 wall_thickness = 10
 
+class Ball:
+    def __init__(self, x, y, radius, color, mass, velocity_x, velocity_y, retention,id):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+        self.velocity_x = velocity_x
+        self.velocity_y = velocity_y
+        self.retention = retention
+        self.id = id
+    
+
+    def draw(self):
+        self.circle = pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.radius)
+
+
 def draw_walls():
     left= pygame.draw.line(screen, (255, 255, 255), (0, 0), (0, HEIGHT), wall_thickness)
     right = pygame.draw.line(screen, (255, 255, 255), (WITH, 0), (WITH, HEIGHT), wall_thickness)
@@ -21,6 +38,9 @@ def draw_walls():
     wall_list = [left, right, top, bottom]
     return wall_list
 
+ball1 = Ball(50, 50,30, (104, 0, 0), 100, 0, 0, 0.9,1)
+ball2 = Ball(500, 500,50, (50, 0, 0), 300, 0, 0, 0.9,2)
+
 #main game loop
 run = True
 while run:
@@ -28,6 +48,8 @@ while run:
     screen.fill((0, 0, 0))
 
     walls = draw_walls()
+    ball1.draw()
+    ball2.draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
